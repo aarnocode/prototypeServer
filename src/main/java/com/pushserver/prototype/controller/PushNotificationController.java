c472b4559db68e917f749df91b0aa5d1d458a624
James Aaron Miranda <aarnocode@gmail.com>
2021-03-25 14:34:45 +0800

    Add subscribe controller

--- src/main/java/com/pushserver/prototype/controller/PushNotificationController.java
+++ src/main/java/com/pushserver/prototype/controller/PushNotificationController.java
@@ -1,9 +1,13 @@
 package com.pushserver.prototype.controller;
 
 import com.google.api.Http;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.TopicManagementResponse;
 import com.pushserver.prototype.model.PushNotificationRequest;
 import com.pushserver.prototype.model.PushNotificationResponse;
 import com.pushserver.prototype.service.PushNotificationService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +15,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -38,5 +45,18 @@ public class PushNotificationController {
                 new PushNotificationResponse(HttpStatus.OK.value(),
                         "Notification has been sent to " + request.getTopic()),
                 HttpStatus.OK);
+    }
+
+    @PostMapping("/subscribe")
+    public String subscribe(@RequestBody PushNotificationRequest request) throws FirebaseMessagingException {
+        List<String> tkn = new ArrayList<String>();
+        tkn.add(request.getToken());
+        try{
+            TopicManagementResponse response = FirebaseMessaging.getInstance().subscribeToTopic(tkn,"all");
+            return "Subscribed to ALL";
+        }catch(Exception e){
+            return e.getMessage();
+        }
+
     }
 }
