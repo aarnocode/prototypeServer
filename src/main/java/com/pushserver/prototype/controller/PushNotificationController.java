e9cf4f8b6614a80136f2e21e83021fbcb3d6af37
James Aaron Miranda <aarnocode@gmail.com>
2021-03-26 11:22:55 +0800

    Removed Cross origin

--- src/main/java/com/pushserver/prototype/controller/PushNotificationController.java
+++ src/main/java/com/pushserver/prototype/controller/PushNotificationController.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin("*")
 public class PushNotificationController {
     @Autowired
     private PushNotificationService pushNotificationService;
