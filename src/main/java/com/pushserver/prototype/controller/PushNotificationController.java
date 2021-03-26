4f1acc749bbba42e3cd733a0d810363d575f3230
James Aaron Miranda <aarnocode@gmail.com>
2021-03-26 16:30:26 +0800

    Included crossorigin

--- src/main/java/com/pushserver/prototype/controller/PushNotificationController.java
+++ src/main/java/com/pushserver/prototype/controller/PushNotificationController.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class PushNotificationController {
     @Autowired
     private PushNotificationService pushNotificationService;
